ae1a3c51bcc330873c4b80caf9e7ccca2d5bfeee
Diego De Franco Matos <DieGit0@users.noreply.github.com>
2020-06-04 17:32:02 -0300

    Update 01.py
    second commit

--- 01.py
+++ 01.py
@@ -4,4 +4,4 @@ instrumentos = ['Baixo', 'Bateria', 'Guitarra']
 # Para cada nome na lista de instrumentos
 for instrumento in instrumentos:
 # mostre o nome do instrumento musical
-	print instrumento
+	print (instrumento)
