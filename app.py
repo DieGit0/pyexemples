fcff95a5a57c592e9275440748d1a55caff693f9
Diego De Franco Matos <DieGit0@users.noreply.github.com>
2020-06-09 00:59:35 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Exemplo de programa em Python
 # Uma lista de instrumentos musicais
-instrumentos = ['Baixo', 'Bateria', 'Guitarra','Saxofone','Gaita de Fole','Cajon']
+instrumentos = ['Baixo', 'Bateria', 'Guitarra','Saxofone','Gaita de Fole','Cajon','Xilofone']
 # Para cada nome na lista de instrumentos
 for instrumento in instrumentos:
 # mostre o nome do instrumento musical
